0eff4e6538332f435de63e2ce1851e91bb5ff6c1
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2014-03-07 15:54:36 -0300

    netdb.h: also defining android_gethostbyaddrforiface_proxy
    Fix:
    bionic/libc/netbsd/net/getnameinfo.c:164:4: warning: implicit declaration
    of function 'android_gethostbyaddrforiface_proxy' [-Wimplicit-function-declaration]
    
    Change-Id: I0bdedd486698c9ad4edf62d204264c4d23281b18
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- libc/include/netdb.h
+++ libc/include/netdb.h
@@ -244,6 +244,7 @@ int getaddrinfo(const char *, const char *, const struct addrinfo *, struct addr
 int android_getaddrinfoforiface(const char *, const char *, const struct addrinfo *, const char *, int, struct addrinfo **);
 int getnameinfo(const struct sockaddr *, socklen_t, char *, size_t, char *, size_t, int);
 int android_getnameinfoforiface(const struct sockaddr *, socklen_t, char *, size_t, char *, size_t, int, const char *, int);
+struct hostent	*android_gethostbyaddrforiface_proxy(const void *, socklen_t, int, const char *, int);
 void freeaddrinfo(struct addrinfo *);
 const char	*gai_strerror(int);
 void setnetgrent(const char *);
